20c5d5ab51c90bb62087c4e2503a9ab21c2a573e
Thomas Wang <iothomaswang@gmail.com>
2019-12-06 13:33:38 -0800

    better config jest

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,9 @@
 /* eslint-env jest */
 
-import Adapter from "enzyme-adapter-react-16";
-import { shallow, configure } from "enzyme";
+import { shallow } from "enzyme";
 import React from "react";
 
 import App from "../pages/index.js";
-
-configure({ adapter: new Adapter() });
 
 describe("With Enzyme", () => {
   it("App shows logo", () => {
