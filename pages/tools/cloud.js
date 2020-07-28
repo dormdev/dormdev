eb103ce8ffaac26c225af7c1ef2a4f2c76871138
Thomas Wang <thomas@wang.sh>
2020-07-27 22:08:47 -0700

    syntax

--- pages/tools/cloud.js
+++ pages/tools/cloud.js
@@ -6,7 +6,7 @@ import Header from 'components/Header'
 import Footer from 'components/Footer'
 import Item, { StyledMain } from 'components/Item'
 
-const Cloud () => (
+const Cloud = () => (
   <>
     <DocumentHead />
     <Global>
